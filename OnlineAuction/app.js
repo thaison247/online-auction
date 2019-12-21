8e37a4d9a620a12c190414bbb231bfbf71e42c56
thaison247 <50402981+thaison247@users.noreply.github.com>
2019-12-22 00:50:20 +0700

    chỉnh sửa view chi tiết sản phẩm

--- OnlineAuction/app.js
+++ OnlineAuction/app.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const numeral = require('numeral');
+const moment = require('moment');
 const app = express();
+
 
 app.use(express.static(__dirname + '/public'));
 
 require('./middlewares/locals.mdw')(app);
 require('./middlewares/routes.mdw')(app);
 
+moment.updateLocale('vi', {
+    relativeTime: {
+        future: "trong %s",
+        past: "%s trước",
+        h: "1 giờ",
+        hh: "%d giờ",
+        d: "1 ngày",
+        dd: "%d ngày",
+        M: "1 tháng",
+        MM: "%d tháng",
+        y: "1 năm",
+        yy: "%d năm"
+    }
+});
+
 app.engine('hbs', exphbs({
     defaultLayout: 'main.hbs',
     layoutsDir: 'views/layouts',
+    helpers: {
+        format: val => numeral(val).format('0,0'),
+        moment: (val, yourformat) => moment(val).format(yourformat),
+        relativeTime: (endDay) => moment(endDay).fromNow(true),
+
+    }
 }));
 app.set('view engine', 'hbs');
 
@@ -31,7 +55,13 @@ app.get('/cart', (req, res) => {
 
 
 
+// app.use((err, req, res, next) => {
+//     res.status(500).send('View error on console.');
+// })
 
+// app.use((err, req, res, next) => {
+//     res.status(404).send('View error on console.');
+// })
 
 const PORT = 3000;
 app.listen(PORT, () => {
--- OnlineAuction/routes/category.route.js
+++ OnlineAuction/routes/category.route.js
@@ -14,11 +14,37 @@ router.get('/:id/products', async (req, res) => {
         }
     }
 
-    const rows = await productModel.allByCat(req.params.id);
+    const rows = await productModel.allByCatWithInfo(req.params.id);
     res.render('vwShop/allByCat', {
         products: rows,
         empty: rows.length === 0
     });
+});
+
+router.get('/:catId/products/:proId', async (req, res) => {
+    const row = await productModel.single(req.params.proId, req.params.catId);
+    const cat = await productModel.catName(req.params.catId);
+    const seller = await productModel.seller(req.params.proId, req.params.catId);
+    const highestBidderAndPrice = await productModel.highestBidderAndPrice(req.params.proId, req.params.catId);
+
+    if (highestBidderAndPrice.length > 0) {
+        res.render('vwProduct/proDetail', {
+            proInfo: row[0],
+            catName: cat[0],
+            seller: seller[0].ten_nguoi_ban,
+            currentPrice: highestBidderAndPrice[0].currentPrice,
+            highestBidder: highestBidderAndPrice[0].highestBidder,
+            rcmPrice: +highestBidderAndPrice[0].currentPrice + row[0].buoc_gia,
+        });
+    } else {
+        res.render('vwProduct/proDetail', {
+            proInfo: row[0],
+            catName: cat[0],
+            seller: seller[0].ten_nguoi_ban,
+            currentPrice: row[0].gia_khoi_diem,
+            rcmPrice: +row[0].gia_khoi_diem + row[0].buoc_gia,
+        });
+    }
 })
 
 module.exports = router;
