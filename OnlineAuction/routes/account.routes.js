38086131f80ec03152a64bf06591e590362bd43d
ThanhTai1999 <kingarthas1999@gmail.com>
2019-12-29 23:56:14 +0700

    kiểm tra mail đã tồn tại chưa (chức năng đăng kí)

--- OnlineAuction/routes/account.routes.js
+++ OnlineAuction/routes/account.routes.js
@@ -1,40 +1,49 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const userModel = require('../models/user.model');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const userModel = require("../models/user.model");
 // const restrict = require('../middlewares/auth.mdw');
 
 const router = express.Router();
 
-router.get('/login', async (req, res) => {
-    res.render('vwAccount/login', {
-        layout: '../layouts/account.hbs'
+router.get("/login", async (req, res) => {
+    res.render("vwAccount/login", {
+        layout: "../layouts/account.hbs"
     });
-})
-
-router.post('/login', (req, res) => {
-
-})
-
-router.get('/register', async (req, res) => {
-    res.render('vwAccount/register', {
-        layout: '../layouts/account.hbs'
-    })
 });
 
-router.post('/register', async (req, res) => {
+router.post("/login", (req, res) => {});
+
+router.get("/register", async (req, res) => {
+    res.render("vwAccount/register", {
+        layout: "../layouts/account.hbs"
+    });
+});
+
+router.post("/register", async (req, res) => {
+    console.log(req.body.email);
+    var isExistedEmail = await userModel.singleByEmail(req.body.email);
+    console.log(isExistedEmail);
+    if (isExistedEmail.length > 0) {
+        console.log(isExistedEmail);
+        return res.render('vwAccount/register', {
+            layout: "../layouts/account.hbs",
+            err_message: 'Thất bại! Email này đã được sử dụng!'
+        })
+    }
     const N = 10;
     const hash = bcrypt.hashSync(req.body.raw_password, N);
-
-    const entity = req.body;
+    var entity = req.body;
     entity.password = hash;
     entity.phan_he = 0;
-    // entity.f_DOB = dob;
+
 
     delete entity.raw_password;
     delete entity.dob;
 
     const result = await userModel.add(entity);
-    res.render('vwAccount/register');
-})
+    res.render("vwAccount/login", {
+        layout: "../layouts/account.hbs"
+    });
+});
 
 module.exports = router;
