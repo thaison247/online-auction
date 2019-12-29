0f223b3d2b401be1ebcd03359ac1d566d932bdb8
thaison247 <50402981+thaison247@users.noreply.github.com>
2019-12-29 20:26:26 +0700

    mã hoá mật khẩu từ form register bằng bcrypt

--- OnlineAuction/app.js
+++ OnlineAuction/app.js
@@ -1,17 +1,32 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const hbs_sections = require('express-handlebars-sections');
-const numeral = require('numeral');
-const moment = require('moment');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const hbs_sections = require("express-handlebars-sections");
+const session = require("express-session");
+const numeral = require("numeral");
+const moment = require("moment");
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
-app.use(express.static(__dirname + '/public'));
+require("./middlewares/locals.mdw")(app);
+require("./middlewares/routes.mdw")(app);
 
-require('./middlewares/locals.mdw')(app);
-require('./middlewares/routes.mdw')(app);
+app.use(
+    session({
+        secret: "keyboard cat",
+        resave: false,
+        saveUninitialized: true
+        // cookie: {
+        //     secure: true
+        // }
+    })
+);
 
-moment.updateLocale('vi', {
+moment.updateLocale("vi", {
     relativeTime: {
         future: "trong %s",
         past: "%s trước",
@@ -26,37 +41,36 @@ moment.updateLocale('vi', {
     }
 });
 
-app.engine('hbs', exphbs({
-    defaultLayout: 'main.hbs',
-    layoutsDir: 'views/layouts',
-    helpers: {
-        section: hbs_sections(),
-        format: val => numeral(val).format('0,0'),
-        moment: (val, yourformat) => moment(val).format(yourformat),
-        relativeTime: (endDay) => moment(endDay).fromNow(true),
+app.engine(
+    "hbs",
+    exphbs({
+        defaultLayout: "main.hbs",
+        layoutsDir: "views/layouts",
+        helpers: {
+            section: hbs_sections(),
+            format: val => numeral(val).format("0,0"),
+            moment: (val, yourformat) => moment(val).format(yourformat),
+            relativeTime: endDay => moment(endDay).fromNow(true)
+        }
+    })
+);
+app.set("view engine", "hbs");
 
-    }
-}));
-app.set('view engine', 'hbs');
-
-app.get('/', (req, res) => {
-    res.render('index');
-})
-
-app.get('/shop', (req, res) => {
-    res.render('shop');
-})
-
-app.get('/product', (req, res) => {
-    res.render('product');
-})
-
-app.get('/cart', (req, res) => {
-    res.render('cart');
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
+app.get("/shop", (req, res) => {
+    res.render("shop");
+});
 
+app.get("/product", (req, res) => {
+    res.render("product");
+});
 
+app.get("/cart", (req, res) => {
+    res.render("cart");
+});
 
 // app.use((err, req, res, next) => {
 //     res.status(500).send('View error on console.');
@@ -69,4 +83,4 @@ app.get('/cart', (req, res) => {
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
-})
+});
--- OnlineAuction/middlewares/routes.mdw.js
+++ OnlineAuction/middlewares/routes.mdw.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
     // app.use('/account', require('../routes/account.route'));
     app.use('/categories', require('../routes/category.route'));
-    app.use('/admin/categories', require('../routes/admin/category.route'));
+    // app.use('/admin/categories', restrict, require('../routes/admin/category.route'));
     app.use('/account', require('../routes/account.route'));
 };
